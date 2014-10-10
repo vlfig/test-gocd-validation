e5cb1cf85342dc0acca7a7db033ecdc957786d39
Vasco Figueira <vasco.figueira@gamesys.co.uk>
2014-10-10 17:53:28 +0100

    Accept files as command line args. Produce executable minimised jar.

--- src/main/java/vlfig/gocd/validation/ConfigValidator.java
+++ src/main/java/vlfig/gocd/validation/ConfigValidator.java
@@ -19,23 +19,36 @@ import com.thoughtworks.go.plugin.infra.PluginManager;
 
 public class ConfigValidator {
 
-    private static final String READ_FROM = "config.xml";
-    private static final String WRITE_TO = "cruise-config.xml";
+    private static final String READ_FROM_DEFAULT = "config.xml";
+    private static final String WRITE_TO_DEFAULT = "cruise-config.xml";
 
     private ConfigValidator() {}
 
-    public static void validateConfig() throws Exception {
+    public static void validateConfig(String... args) throws Exception {
+        String readFrom = READ_FROM_DEFAULT;
+        String writeTo = WRITE_TO_DEFAULT;
+
+        if(args.length > 0) {
+            readFrom = args[0];
+            if(args.length > 1) {
+                writeTo = args[1];
+            }
+        }
+        validateConfig(readFrom, writeTo);
+    }
+
+    public static void validateConfig(String readFrom, String writeTo) throws Exception {
         MetricsProbeService metricsProbeService = mock(MetricsProbeService.class);
         ConfigElementImplementationRegistry registryWithNoPlugins = registryWithNoPlugins();
 
         MagicalGoConfigXmlLoader loader =
                 new MagicalGoConfigXmlLoader(new ConfigCache(), registryWithNoPlugins, metricsProbeService);
-        String configStr = new String(readAllBytes(get(READ_FROM)));
+        String configStr = new String(readAllBytes(get(readFrom)));
         CruiseConfig config = loader.loadConfigHolder(configStr).config;
 
         MagicalGoConfigXmlWriter writer =
                 new MagicalGoConfigXmlWriter(new ConfigCache(), registryWithNoPlugins, metricsProbeService);
-        OutputStream os = Files.newOutputStream(get(WRITE_TO));
+        OutputStream os = Files.newOutputStream(get(writeTo));
         writer.write(config, os, true);
     }
 
@@ -47,6 +60,6 @@ public class ConfigValidator {
     }
 
     public static void main(String[] args) throws Exception {
-        validateConfig();
+        validateConfig(args);
     }
 }
